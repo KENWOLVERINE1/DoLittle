e2ebfefc4705a5dbb78d0052b3b27c26f5cac7a0
KENWOLVERINE1 <kenwolverine94@gmail.com>
2024-04-07 14:34:00 +0530

    Initiating add_task function and cleaning up

--- lib/screens/add_task.dart
+++ lib/screens/add_task.dart
@@ -14,18 +14,27 @@ class _AddTaskState extends State<AddTask> {
     return Scaffold(
       appBar: AppBar(title: Text('New Task',style: GoogleFonts.poppins(fontSize: 25,color: Colors.white),),backgroundColor: Colors.deepPurple.shade300,),
       body: Container(
-        padding: EdgeInsets.all(20),
+        padding: const EdgeInsets.all(20),
         child: Column(
           children: [
-            Container(child: TextField(
+            Container(child: const TextField(
               decoration: InputDecoration(labelText: 'Enter Task',border: OutlineInputBorder()),
             ),
             ),
-            SizedBox(height: 10,),
-            Container(child: TextField(
+            const SizedBox(height: 10,),
+            Container(child: const TextField(
               decoration: InputDecoration(labelText: 'Enter Description',border: OutlineInputBorder()),
             ),
-            )
+            ),
+            Container(
+                width: double.infinity,
+               height: 40,
+               child: ElevatedButton(onPressed: (){}, style: ButtonStyle(backgroundColor: MaterialStateProperty.all(const Color.fromRGBO(83, 58, 113, 1)),padding: MaterialStateProperty.resolveWith<EdgeInsetsGeometry>(
+                     (Set<MaterialState> states) {
+                   return const EdgeInsets.all(45);
+                 },
+               ),),
+               child: Text('Add Task',style: GoogleFonts.poppins(fontSize: 20,color: Colors.white)),))
           ],
         ),
       ),
